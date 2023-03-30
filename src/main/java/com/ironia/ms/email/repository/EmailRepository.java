983124d059237c4e76fe86b9979993a18cff374d
r41l1m4 <rai.lima17@gmail.com>
2023-03-30 20:23:02 -0300

    model data type change

--- src/main/java/com/ironia/ms/email/repository/EmailRepository.java
+++ src/main/java/com/ironia/ms/email/repository/EmailRepository.java
@@ -3,5 +3,7 @@ package com.ironia.ms.email.repository;
 import com.ironia.ms.email.model.EmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailRepository extends JpaRepository<EmailModel, Long> {
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
 }
